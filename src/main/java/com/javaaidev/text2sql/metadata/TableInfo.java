a9d4f57135e031ed2b38bc3481ff1a1bcfa43bf1
Fu Cheng <alexcheng1982@gmail.com>
2025-05-03 23:41:50 +1200

    Upgrade to Spring AI 1.0.0-M7

--- src/main/java/com/javaaidev/text2sql/metadata/TableInfo.java
+++ src/main/java/com/javaaidev/text2sql/metadata/TableInfo.java
@@ -1,7 +1,10 @@
 package com.javaaidev.text2sql.metadata;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.util.List;
 
+@JsonInclude(Include.NON_ABSENT)
 public record TableInfo(
     String name,
     String description,
